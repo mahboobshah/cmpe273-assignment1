29635db61408f270f09a829c68f1fe255dd88654
Mahboob Shah Mohammed <mahboobshah16@gmail.com>
2013-09-25 06:40:46 -0700

    Initial commit

---
+++ src/main/java/edu/sjsu/cmpe/library/domain/Book.java
@@ -0,0 +1,114 @@
+package edu.sjsu.cmpe.library.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class Book {
+    private long isbn;
+    private String title;
+    private String status;
+    @JsonProperty("publication-date")
+    private String publicationDate;
+    private String language;
+    @JsonProperty("num-pages")
+    private long numPages;
+    @JsonProperty("authors")
+    private ArrayList<Author> authors;
+    //Author auth1 = new Author( ArrayList<Author> authors;
+    
+
+    // add more fields here
+
+    /**
+     * @return the isbn
+     */
+    public long getIsbn() {
+	return isbn;
+    }
+
+    /**
+     * @param isbn
+     *            the isbn to set
+     */
+    public void setIsbn(long isbn) {
+	this.isbn = isbn;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+	return title;
+    }
+
+    /**
+     * @param title
+     *            the title to set
+     */
+    public void setTitle(String title) {
+	this.title = title;
+    }
+    
+    
+
+	/**
+	 * @return the language
+	 */
+	public String getLanguage() {
+		return language;
+	}
+
+	/**
+	 * @param language 
+	 * 			the language to set
+	 */
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status 
+	 * 			the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the authors
+	 */
+	public ArrayList<Author> getAuthors() {
+		return authors;
+	}
+
+	/**
+	 * @param authors the authors to set
+	 */
+	public void setAuthors(ArrayList<Author> authors) {
+		this.authors = authors;
+	}
+
+	/**
+	 * @return the publicationDate
+	 */
+	public String getPublicationDate() {
+		return publicationDate;
+	}
+
+	/**
+	 * @param publicationDate the publicationDate to set
+	 */
+	public void setPublicationDate(String publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+}
---
+++ src/main/java/edu/sjsu/cmpe/library/domain/Review.java
@@ -0,0 +1,52 @@
+package edu.sjsu.cmpe.library.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Review {
+	private Integer id;
+	private Integer rating;
+	@JsonProperty("comment")
+	private String comment;
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @return the rating
+	 */
+	public Integer getRating() {
+		return rating;
+	}
+	
+	/**
+	 * @param rating the rating to set
+	 */
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+	
+	/**
+	 * @return the comment
+	 */
+	public String getComment() {
+		return comment;
+	}
+	
+	/**
+	 * @param comments the comments to set
+	 */
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+}
